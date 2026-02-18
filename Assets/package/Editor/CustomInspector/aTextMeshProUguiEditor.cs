80c2f9f9a14fd00d531742a5ee338c563eb018fc
Mikan <mikan@a-nest.pink>
2026-02-19 01:21:33 +0900

    ・aTextMeshProのローカライズ対応完了、ルビ関連処理は一旦削除

--- Assets/package/Editor/CustomInspector/aTextMeshProUguiEditor.cs
+++ Assets/package/Editor/CustomInspector/aTextMeshProUguiEditor.cs
@@ -5,13 +5,12 @@ using UnityEditor;
 using UnityEditor.Localization;
 using UnityEngine;
 using UnityEngine.Localization.Tables;
-
 namespace ANest.UI {
 	/// <summary>aTextMeshProUguiのLocalization対応カスタムインスペクタ。</summary>
 	[CustomEditor(typeof(aTextMeshProUgui))]
 	public class aTextMeshProUguiEditor : UnityEditor.Editor {
 		#region Fields
-		private SerializedProperty m_stringTableProp;    // StringTable参照プロパティ
+		private SerializedProperty m_stringTableProp;    // StringTable 参照プロパティ
 		private SerializedProperty m_localizationKeyProp; // Localizationキープロパティ
 		private StringTableCollection[] m_tableCollections; // 全StringTableCollection
 		private string[] m_tableNames;                      // テーブル名一覧
@@ -20,69 +19,43 @@ namespace ANest.UI {
 		private int m_selectedKeyIndex;                     // 選択中キーインデックス
 		private string m_keySearchFilter = "";              // キー検索フィルタ
 		private string[] m_filteredKeys;                    // フィルタ済みキー一覧
-		private SerializedProperty m_rawTextProp;          // ルビタグ付き元テキストプロパティ
 		private UnityEditor.Editor m_tmpEditor;             // TMP標準エディタ
 		private static Type s_tmpEditorType;                // TMP_EditorPanelUI型キャッシュ
 		#endregion
-
 		#region Unity Methods
 		private void OnEnable() {
 			m_stringTableProp = serializedObject.FindProperty("m_stringTable");
 			m_localizationKeyProp = serializedObject.FindProperty("m_localizationKey");
-			m_rawTextProp = serializedObject.FindProperty("m_rawText");
 			RefreshTableCollections();
 			SyncSelectedTableIndex();
 			RefreshKeys();
 			SyncSelectedKeyIndex();
 			CreateTmpEditor();
 		}
-
 		private void OnDisable() {
 			if(m_tmpEditor != null) {
 				DestroyImmediate(m_tmpEditor);
 				m_tmpEditor = null;
 			}
 		}
-
 		public override void OnInspectorGUI() {
 			serializedObject.Update();
-
 			EditorGUILayout.LabelField("Localization", EditorStyles.boldLabel);
 			DrawTableDropdown();
 			DrawKeySearchField();
 			DrawKeyDropdown();
-
 			if(serializedObject.ApplyModifiedProperties()) {
 				foreach(var t in targets) {
 					var tmp = (aTextMeshProUgui)t;
 					tmp.LocalizationKey = tmp.LocalizationKey;
 				}
 			}
-
 			EditorGUILayout.Space();
-
-			// TMP標準インスペクタのテキスト変更を検知してルビ解析を実行
 			if(m_tmpEditor != null) {
-				EditorGUI.BeginChangeCheck();
 				m_tmpEditor.OnInspectorGUI();
- 			if(EditorGUI.EndChangeCheck()) {
-					foreach(var t in targets) {
-						var tmp = (aTextMeshProUgui)t;
-						// TMPエディタがm_textを直接書き換えるので、textプロパティ経由でルビ解析に通す
-						var editedText = tmp.GetParsedText() ?? "";
-						// m_textから直接読み取り（TMPエディタが書き換えた値）
-						serializedObject.Update();
-						var tmProp = serializedObject.FindProperty("m_text");
-						if(tmProp != null) editedText = tmProp.stringValue;
-						tmp.text = editedText;
-						m_rawTextProp.stringValue = tmp.RawText;
-						serializedObject.ApplyModifiedPropertiesWithoutUndo();
-					}
-				}
 			}
 		}
 		#endregion
-
 		#region Private Methods
 		/// <summary>TMP標準エディタを生成する。</summary>
 		private void CreateTmpEditor() {
@@ -93,7 +66,6 @@ namespace ANest.UI {
 				m_tmpEditor = CreateEditor(targets, s_tmpEditorType);
 			}
 		}
-
 		/// <summary>StringTableCollectionの一覧を取得する。</summary>
 		private void RefreshTableCollections() {
 			m_tableCollections = LocalizationEditorSettings.GetStringTableCollections().ToArray();
@@ -101,7 +73,6 @@ namespace ANest.UI {
 			names.AddRange(m_tableCollections.Select(c => c.TableCollectionName));
 			m_tableNames = names.ToArray();
 		}
-
 		/// <summary>現在のSerializedPropertyからテーブル選択インデックスを同期する。</summary>
 		private void SyncSelectedTableIndex() {
 			m_selectedTableIndex = 0;
@@ -116,7 +87,6 @@ namespace ANest.UI {
 				}
 			}
 		}
-
 		/// <summary>選択テーブルのキー一覧を取得する。</summary>
 		private void RefreshKeys() {
 			if(m_selectedTableIndex <= 0 || m_selectedTableIndex > m_tableCollections.Length) {
@@ -128,7 +98,6 @@ namespace ANest.UI {
 			m_keys = collection.SharedData.Entries.Select(e => e.Key).OrderBy(k => k).ToArray();
 			ApplyKeyFilter();
 		}
-
 		/// <summary>現在のLocalizationKeyからキー選択インデックスを同期する。</summary>
 		private void SyncSelectedKeyIndex() {
 			m_selectedKeyIndex = 0;
@@ -142,7 +111,6 @@ namespace ANest.UI {
 				}
 			}
 		}
-
 		/// <summary>検索フィルタをキー一覧に適用する。</summary>
 		private void ApplyKeyFilter() {
 			if(string.IsNullOrEmpty(m_keySearchFilter)) {
@@ -152,7 +120,6 @@ namespace ANest.UI {
 				m_filteredKeys = m_keys.Where(k => k.ToLowerInvariant().Contains(lower)).ToArray();
 			}
 		}
-
 		/// <summary>StringTableドロップダウンを描画する。</summary>
 		private void DrawTableDropdown() {
 			var newIndex = EditorGUILayout.Popup("String Table", m_selectedTableIndex, m_tableNames);
@@ -164,7 +131,6 @@ namespace ANest.UI {
 				m_localizationKeyProp.stringValue = "";
 			}
 		}
-
 		/// <summary>キー検索フィールドを描画する。</summary>
 		private void DrawKeySearchField() {
 			var newFilter = EditorGUILayout.TextField("Key Search", m_keySearchFilter);
@@ -174,7 +140,6 @@ namespace ANest.UI {
 				SyncSelectedKeyIndex();
 			}
 		}
-
 		/// <summary>キードロップダウンを描画する。</summary>
 		private void DrawKeyDropdown() {
 			if(m_filteredKeys == null || m_filteredKeys.Length == 0) {
@@ -187,7 +152,6 @@ namespace ANest.UI {
 				m_localizationKeyProp.stringValue = m_filteredKeys[newKeyIndex];
 			}
 		}
-
 		/// <summary>選択されたテーブルをSerializedPropertyに反映する。</summary>
 		private void ApplySelectedTable() {
 			var tableRefProp = m_stringTableProp.FindPropertyRelative("m_TableReference");
